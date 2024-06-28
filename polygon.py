ca77a38c068a33a2bd61f163c8535a7748cd1c28
eleora-h <eleora.hamming@gmail.com>
2024-06-27 21:46:51 -0400

    Added in credentials file and base class for calling API

--- polygon.py
+++ polygon.py
@@ -1,5 +1,25 @@
-import matplotlib
+import requests
+import retrying
+import logging
+
+# https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksticker__range__multiplier___timespan___from___to
 
 class Polygon():
+    def __init__(self):
+        self.auth_token = self.__get_authentication_key()
+        self.__configure_logger()
+
+    def __get_authentication_key(self):
+        reader = open('credentials')
+        reader = reader.read()
+        key = reader.split(":")[1]
+        return key
+
+    def __configure_logger(self):
+        pass
+
+    def __send_request(self):
+        pass
+
     def run(self):
-        print('hello world')
+        self.__get_authentication_key()
